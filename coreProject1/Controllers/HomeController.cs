893fa185ff2e7bbcb9dd2d3b0f1c38002ffed4d6
CHANG HUA Huang <bradford.hunge@gmail.com>
2022-10-30 10:36:20 +0800

    20221030 bradford complete

---
+++ coreProject1/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace coreProject1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.DateTime = System.DateTime.Now.ToString("yyyy/MM/dd");
+            ViewBag.test = "test";
+
+
+            return View();
+        }
+    }
+}
--- coreProject1/Program.cs
+++ coreProject1/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMvc();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.MapControllerRoute(name: "default", pattern: "{Controller=Home}/{Action=Index}/{id?}");
 
 app.Run();
